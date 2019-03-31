ac0b20fa0f56b1177642ac32d43946114a681347
darkintruder0 <ing.allan.flores@gmail.com>
2019-03-30 20:39:39 -0600

    Crypto en download

--- sgdadmin/src/main/java/com/company/sgdadmin/serviceimp/FileManagerImpl.java
+++ sgdadmin/src/main/java/com/company/sgdadmin/serviceimp/FileManagerImpl.java
@@ -8,6 +8,7 @@ import com.company.sgdadmin.dto.filemanager.FileManagerDTO;
 import com.company.sgdadmin.entity.DocumentosActivosEntity;
 import com.company.sgdadmin.repository.DocumentosActivosRepository;
 import com.company.sgdadmin.service.FileManager;
+import com.company.sgdadmin.util.CryptoFiles;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;    
@@ -39,6 +40,9 @@ public class FileManagerImpl implements FileManager {
 
     @Autowired
     HttpServletResponse response;
+    
+    @Autowired
+    CryptoFiles cryptoFiles;
 
     //private static final Logger LOGGER = Logger.getLogger(FileManagerImpl.class);
     public void uploading(FileManagerDTO dto) throws IOException {
@@ -55,7 +59,7 @@ public class FileManagerImpl implements FileManager {
      * @throws IOException
      */
     @Override
-    public void downloadFile(DocumentosActivosEntity entidad ) throws IOException {
+    public void downloadFile(DocumentosActivosEntity entidad) throws IOException {
         //String rootPath = ConstantsSGD.HOME; validar si se incluye la ruta absoluta o la relativa
         File file = new File(entidad.getRuta() + File.separator + entidad.nombre);
         String mimeType = URLConnection.guessContentTypeFromName(file.getName());
@@ -66,26 +70,31 @@ public class FileManagerImpl implements FileManager {
         if (!file.exists()) {
             String errorMessage = "Sorry. The file you are looking for does not exist";
             System.out.println(errorMessage);
-            OutputStream outputStream = response.getOutputStream();
-            outputStream.write(errorMessage.getBytes(Charset.forName("UTF-8")));
-            outputStream.close();
+            try (OutputStream outputStream = response.getOutputStream()) {
+                outputStream.write(errorMessage.getBytes(Charset.forName("UTF-8")));
+            }
             return;
         }
-        System.out.println("mimetype : " + mimeType);
 
-        response.setContentType(mimeType);
+        file = cryptoFiles.processFileEncrypt(file, true);
 
-        /* "Content-Disposition : inline" will show viewable types [like images/text/pdf/anything viewable by browser] right on browser 
+        if (file != null) {
+            System.out.println("mimetype : " + mimeType);
+
+            response.setContentType(mimeType);
+
+            /* "Content-Disposition : inline" will show viewable types [like images/text/pdf/anything viewable by browser] right on browser 
             while others(zip e.g) will be directly downloaded [may provide save as popup, based on your browser setting.]*/
-        response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() + "\""));
+            response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() + "\""));
 
-        /* "Content-Disposition : attachment" will be directly download, may provide save as popup, based on your browser setting*/
-        //response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
-        response.setContentLength((int) file.length());
+            /* "Content-Disposition : attachment" will be directly download, may provide save as popup, based on your browser setting*/
+            //response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+            response.setContentLength((int) file.length());
 
-        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
 
-        //Copy bytes from source to destination(outputstream in this example), closes both streams.
-        FileCopyUtils.copy(inputStream, response.getOutputStream());
+            //Copy bytes from source to destination(outputstream in this example), closes both streams.
+            FileCopyUtils.copy(inputStream, response.getOutputStream());
+        }
     }
 }
